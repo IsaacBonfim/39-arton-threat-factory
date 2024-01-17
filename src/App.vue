eccdd4dffcf8ee9425c425c6cce81acbfccaba78
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2024-01-17 02:57:03 -0300

    Adicionado o Header da aplicação e criado alguns estilos globais

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,76 @@
 <template>
+  <VueHeader />
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">Criador de Ameaças</router-link> |
   </nav>
   <router-view/>
 </template>
 
+<script>
+import VueHeader from './components/VueHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    VueHeader
+  }
+}
+</script>
+
 <style>
+@font-face {
+  font-family: 'tormenta';
+  src: url('./fonts/Tormenta20.ttf') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+  width: 100vw;
+}
+
 #app {
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+  align-items: center;
+  border-bottom: 4px inset rgb(68, 2, 0);
+  display: flex;
+  height: 6vh;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  align-items: center;
+  color: rgb(168, 20, 0);
+  display: flex;
+  font-family: 'tormenta';
+  font-size: 1.5em;
+  height: 100%;
+  justify-content: center;
+  padding: 10px;
+  text-decoration: none;
+  width: 10%;
+}
+
+nav a:hover {
+  background-color: rgba(248, 232, 202, 0.62);
+  color: rgb(128, 20, 0);
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: rgb(68, 2, 0);
 }
 </style>
