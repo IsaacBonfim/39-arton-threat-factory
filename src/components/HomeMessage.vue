dc4b29ff0bfe08db97c695c9f221ca9cd51deba5
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2024-01-21 23:55:17 -0300

    Adicionado texto definitivo da tela inicial

--- src/components/HomeMessage.vue
+++ src/components/HomeMessage.vue
@@ -1,35 +1,18 @@
 <template>
   <div class="home-box">
-    <h1>{{ msg }}</h1>
     <img class="home-img" alt="Tormenta" src="../assets/Tormenta.png">
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    <p class="home-txt">
+      Bem vindo a <span class="spotlight">Fábrica de Ameaças de Arton</span> um projeto para auxiliar na criação de ameaças para o RPG de mesa <span class="spotlight">Tormenta 20</span>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p class="home-txt">
+      Para todos que já jogaram Tormenta 20 e se aventuraram por Arton, sabem que essa terra fantástica é repleta dos mais diversos tipos de perigos, mas mesmo assim, as vezes nos surge uma ideia que parece não se encaixar em nenhuma das varias ameaças já existentes, e então o que nos resta é utilizar a nossa criatividade para dar forma a nossas ideias.
+    </p>
+    <p class="home-txt">
+      O processo criar uma nova ameaça por vezes pode ser trabalhoso e a ideia por trás deste projeto é exatamente reduzir um pouco deste trabalho.
+    </p>
+    <p class="home-txt">
+      Sendo assim, espero que aproveite a ferramenta e tenha uma boa experiencia.
+    </p>
   </div>
 </template>
 
@@ -42,10 +25,16 @@ export default {
 <style scoped>
 
 .home-box {
-  height: 65vh;
+  background-image: url('../assets/PageBG.svg');
+  background-size: cover;
+  border-radius: 50px;
+  box-shadow: 20px 20px 60px rgb(217, 217, 217),
+    -20px -20px 60px rgb(255, 255, 255);
+  height: fit-content;
   margin: 2vb;
-  padding: 2vh 1vw;
-  width: 35vw;
+  min-height: 900px;
+  /* padding: 2vh 1vw; */
+  width: 45vw;
 }
 
 .home-img {
@@ -54,18 +43,16 @@ export default {
   width: 50%;
 }
 
-h3 {
-  margin: 40px 0 0;
+.home-txt {
+  background-color: rgba(255, 255, 255, 0.7);
+  color: rgb(68, 2, 0);
+  font-size: 1.3em;
+  padding: 1.5vb;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.spotlight {
+  color: rgb(168, 20, 0);
+  font-family: 'Tormenta'
 }
 </style>
